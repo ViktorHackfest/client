e8f2859bf40505c5a6ed562ff0a30a0cfb42358e
eugeniusms <eugeniusms@gmail.com>
2023-02-27 13:41:33 +0700

    🛠 Semifinal Navbar (Minus Styling and Dropdown State)

--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -1,3 +1,48 @@
+import { fetchUser, userAccessToken } from '@utils/fetchGoogleAuth';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { BsList } from 'react-icons/bs';
+
+type User = {
+  photoURL: string;
+  displayName: string;
+  email: string;
+};
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const router = useRouter();
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    (async () => {
+      const accessToken = userAccessToken();
+      if (!accessToken) return router.push('/auth/login');
+      const [userInfo] = fetchUser();
+      console.log(userInfo);
+      setUser(userInfo);
+    })();
+  }, []);
+
+  return (
+    <div className="flex justify-between border-b-2 border-b-black py-4 px-4">
+      <Image
+        src="/assets/images/logo-navbar-tripfolk.svg"
+        alt=""
+        width={100}
+        height={100}
+      />
+      <div className="flex items-center gap-4">
+        <Image
+          src={user?.photoURL}
+          className="rounded-full"
+          alt=""
+          width={30}
+          height={30}
+        />
+        <div className="text-lg">Hi, {user?.displayName}</div>
+        <BsList size={26} />
+      </div>
+    </div>
+  );
 };
--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const withReactSvg = require('next-react-svg');
-const path = require('path');
-
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, 'public/assets'),
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: [
       'localhost',
@@ -11,10 +9,6 @@ module.exports = withReactSvg({
       'lh3.googleusercontent.com',
     ],
   },
-  experimental: {
-    esmExternals: false,
-  },
-  webpack(config, options) {
-    return config;
-  },
-});
+};
+
+module.exports = nextConfig;
