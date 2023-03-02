0a2037f2ce2a35b22b38857584d3b0322c015eab
eugeniusms <eugeniusms@gmail.com>
2023-03-02 18:33:16 +0700

    🔨 Add Request Method on API Call

--- pages/api/travel/cities/index.ts
+++ pages/api/travel/cities/index.ts
@@ -3,29 +3,33 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { City } from '@models/City';
 import axios from 'axios';
 
-export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  axios
-    .get(`${process.env.NEXT_PUBLIC_DEPLOY_SERVER_DEVELOPMENT}/travel/cities/`)
-    .then((response) => {
-      // console.log(response.data);
-      try {
-        res.status(200).json(
-          response.data.map(
-            (city: any) =>
-              ({
-                id: city.id,
-                name: city.name,
-                province: city.province,
-                lat: city.lat,
-                lng: city.lng,
-              } as City)
-          )
-        );
-      } catch (e) {
-        console.log(e);
-      }
-    })
-    .catch((error) => res.status(500).send(error));
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_DEPLOY_SERVER_DEVELOPMENT}/travel/cities/`
+      )
+      .then((response) => {
+        // console.log(response.data);
+        try {
+          res.status(200).json(
+            response.data.map(
+              (city: any) =>
+                ({
+                  id: city.id,
+                  name: city.name,
+                  province: city.province,
+                  lat: city.lat,
+                  lng: city.lng,
+                } as City)
+            )
+          );
+        } catch (e) {
+          console.log(e);
+        }
+      })
+      .catch((error) => res.status(500).send(error));
+  }
 }
 
 export const config = {
