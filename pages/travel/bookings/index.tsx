afdef1d2aae3839550cbc01bf80e0ecc2dbd4c18
eugeniusms <eugeniusms@gmail.com>
2023-03-03 10:23:17 +0700

    🔨 Bookings Pages Done

--- pages/travel/bookings/index.tsx
+++ pages/travel/bookings/index.tsx
@@ -1,27 +1,27 @@
-import { CitiesContainer } from '@components/modules/travel/cities';
-import { City } from '@models/City';
+import { BookingsContainer } from '@components/modules/travel/bookings';
+import { Booking } from '@models/Booking';
 import { LoadingScreen } from '@ui';
 import axios from 'axios';
 import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 
-const Cities: NextPage = () => {
-  // GET : api/travel/cities
-  const [cities, setCities] = useState<City>();
-  const [citiesLoading, setCitiesLoading] = useState(true);
+const Bookings: NextPage = () => {
+  // GET : api/travel/bookings
+  const [bookings, setbookings] = useState<Booking>();
+  const [bookingsLoading, setbookingsLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('/api/travel/cities').then((response) => {
-      setCities(response.data);
-      setCitiesLoading(false);
+    axios.get('/api/travel/bookings').then((response) => {
+      setbookings(response.data);
+      setbookingsLoading(false);
     });
   }, []);
 
-  if (citiesLoading) {
+  if (bookingsLoading) {
     return <LoadingScreen />;
   } else {
-    return <CitiesContainer cities={cities} />;
+    return <BookingsContainer bookings={bookings} />;
   }
 };
 
-export default Cities;
+export default Bookings;
