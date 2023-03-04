a8d16b58e698f8158f2489f918035d3baf6f2993
eugeniusms <eugeniusms@gmail.com>
2023-03-04 08:43:59 +0700

    🔨 SearchDestination SemiFinal API Connected

--- components/modules/home/SearchDestination.tsx
+++ components/modules/home/SearchDestination.tsx
@@ -1,3 +1,32 @@
-export const SearchDestination = () => {
-  return <div>SEARCH</div>;
+import { Card, InputField } from '@ui';
+import { IoLocationSharp } from 'react-icons/io5';
+
+export const SearchDestination = ({ destinations }: any) => {
+  console.log(destinations.length);
+
+  let x = Math.floor(Math.random() * destinations.length); // Random From 0 to destinations.length
+  let y = Math.floor(Math.random() * destinations.length); // Random From 0 to destinations.length
+  const recomendation = [destinations[x], destinations[y]];
+
+  return (
+    <Card preset="dark" className="rounded-none">
+      <div>
+        <div>
+          <IoLocationSharp />
+          <p>Choose Destination</p>
+        </div>
+        <div>
+          <InputField placeholder="Search Destination" />
+        </div>
+        {/* STATIC FIRST */}
+        <div>
+          <span>Place recommendation: </span>
+          {recomendation.map((rec: any, id_rec: number) => (
+            <span key={id_rec}>{rec.name},&nbsp;</span>
+          ))}
+          ...
+        </div>
+      </div>
+    </Card>
+  );
 };
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -3,21 +3,37 @@ import {
   SearchDestination,
   ShortcutButton,
 } from '@components/modules/home';
+import { Destination } from '@models/Destination';
 import { Footer, Navbar } from '@ui';
-import { useRouter } from 'next/router';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
-  const router = useRouter();
-  const routeToLogin = () => {
-    router.push('/auth/login');
-  };
-  return (
-    <div className="h-minscreen bg-slate-800">
-      <Navbar />
-      <SearchDestination />
-      <ShortcutButton />
-      <NearestTourGuide />
-      <Footer />
-    </div>
-  );
+  // GET : api/travel/destinations
+  const [destinations, setDestinations] = useState<Destination>();
+  const [destinationsLoading, setDestinationsLoading] = useState(true);
+  const [isDestinationsEmpty, setIsDestinationsEmpty] = useState(false);
+
+  useEffect(() => {
+    axios.get('/api/travel/destinations').then((response) => {
+      setDestinations(response.data);
+      console.log(response.data);
+      setDestinationsLoading(false);
+      if (response.data.length === 0) setIsDestinationsEmpty(true);
+    });
+  }, []);
+
+  if (destinationsLoading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="h-minscreen bg-slate-800">
+        <Navbar />
+        <SearchDestination destinations={destinations} />
+        <ShortcutButton />
+        <NearestTourGuide />
+        <Footer />
+      </div>
+    );
+  }
 }
