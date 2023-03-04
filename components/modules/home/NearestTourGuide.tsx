0a492e688bec703d974aadd01333849fc05b52cb
eugeniusms <eugeniusms@gmail.com>
2023-03-04 12:52:44 +0700

    🛠 Add Card Accessories

--- components/modules/home/NearestTourGuide.tsx
+++ components/modules/home/NearestTourGuide.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { TourGuideCard } from './TourGuideCard';
 
 export const NearestTourGuide = ({ tour_guides }: any) => {
@@ -6,7 +7,9 @@ export const NearestTourGuide = ({ tour_guides }: any) => {
     <div className="bg-white px-10 pb-20">
       <div className="flex justify-between items-center py-4">
         <div className="text-xl font-semibold">Nearest Tour Guide</div>
-        <div className="text-md text-gray-500 underline">See All</div>
+        <Link href="/user/tour-guide/">
+          <div className="text-md text-gray-500 underline">See All</div>
+        </Link>
       </div>
       <div className="bg-white flex overflow-x-auto gap-4">
         {tour_guides.map((tour_guide: any, id_tour_guide: number) => (
--- components/modules/home/TourGuideCard.tsx
+++ components/modules/home/TourGuideCard.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { AiFillStar } from 'react-icons/ai';
+import { IoLocationSharp } from 'react-icons/io5';
 
 export const TourGuideCard = ({ tour_guide, key }: any) => {
   return (
@@ -16,7 +18,16 @@ export const TourGuideCard = ({ tour_guide, key }: any) => {
       <div className="p-4">
         <div className="font-bold">Eugenius Mario</div>
         <div className="h-2"></div>
-        <div>
+        <div className="flex gap-4 items-center">
+          <div className="flex items-center gap-[6px]">
+            <AiFillStar className="w-5 h-5 text-yellow-500" />
+            {Math.floor(Math.random() * 1 + 4)}.{Math.floor(Math.random() * 9)}
+            {Math.floor(Math.random() * 9)}
+          </div>
+          <div>{Math.floor(Math.random() * 10000 + 1)} reviews</div>
+        </div>
+        <div className="flex gap-[1px] items-center">
+          <IoLocationSharp className="w-5 h-5" />
           <span>{tour_guide.city.name}</span>
         </div>
         <div>{tour_guide.city.province}</div>
