b022d808bb3a961620eb90a7557a2f33cd8a7842
eugeniusms <eugeniusms@gmail.com>
2023-03-04 12:02:54 +0700

    🛠 Add API Fetch Page For Tour Guide

---
+++ pages/user/tour-guide/index.tsx
@@ -0,0 +1,39 @@
+import { useAuthBackend } from '@hooks';
+import { City } from '@models/City';
+import { Breadcrumb, Footer, LoadingScreen, Navbar } from '@ui';
+import axios from 'axios';
+import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+
+const TourGuide: NextPage = () => {
+  // GET : api/travel/tourGuides
+  const [tourGuides, setTourGuides] = useState<City>();
+  const [tourGuidesLoading, setTourGuidesLoading] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {
+    axios.get('/api/user/tour-guide').then((response) => {
+      setTourGuides(response.data);
+      setTourGuidesLoading(false);
+      console.log(response.data);
+      if (response.data.length === 0) setIsEmpty(true);
+    });
+  }, []);
+
+  useAuthBackend();
+
+  if (tourGuidesLoading) {
+    return <LoadingScreen />;
+  } else {
+    if (isEmpty) return <div>There are no tourGuides</div>;
+    return (
+      <>
+        <Navbar />
+        <Breadcrumb path={['Tour Guide']} />
+        <Footer />
+      </>
+    );
+  }
+};
+
+export default TourGuide;
