d4ef8af8028f62739257cac758dd9daeeec26f27
eugeniusms <eugeniusms@gmail.com>
2023-03-04 11:03:01 +0700

    🔧 Fix Flex Bug

--- components/modules/travel/destinations/DestinationsContainer.tsx
+++ components/modules/travel/destinations/DestinationsContainer.tsx
@@ -9,9 +9,11 @@ export const DestinationsContainer = ({
 }: DestinationsContainerProps) => {
   return (
     <div className="flex justify-center px-8 pb-20">
-      {destinations.map((destination: any, id_destination: number) => (
-        <DestinationCard key={id_destination} destination={destination} />
-      ))}
+      <div className="flex flex-col gap-10">
+        {destinations.map((destination: any, id_destination: number) => (
+          <DestinationCard key={id_destination} destination={destination} />
+        ))}
+      </div>
     </div>
   );
 };
